96414ef7bebd64db83968cc7f12bc2f46ef8ed5f
Ivan Z. Ganza <ivan.ganza@gmail.com>
2011-02-02 16:36:41 -0500

    add "render_views" so that specs can pass.  Otherwise your pages aren't sent back.  It seems to be only a default template sent back by rails.

--- spec/controllers/root_controller_spec.rb
+++ spec/controllers/root_controller_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe RootController do
+  render_views
+
   it "responds to #index" do
     get :index
     assert_response :success
@@ -8,6 +10,9 @@ describe RootController do
 
   it "returns default css stylesheet" do
     get :index
-    
+    ["html", "head", "body"].each{|o| response.should have_tag(o)}
+    ["container", "top", "left_nav", "right_nav", "content", "footer"].each do |o|
+      response.should have_tag("div#" << o)
+    end
   end
 end
